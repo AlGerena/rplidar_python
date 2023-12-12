a9134efe407eba38636b2bc35819295666509b63
AlGerena <alexisgerena@outlook.es>
2023-12-12 14:17:40 -0500

    Blame

--- boost_mode/boost.py
+++ boost_mode/boost.py
@@ -1,3 +1,8 @@
-#uart1.write(bytes([0xA5, 0x82, 0x05, 0x01, 0x00, 0x00, 0x00, 0x00, 0x23]))
-0x28(0x00), 0x28(0x01)
-must be > 0.85 to 0x28(0x01).
+import serial
+from time import sleep
+
+uart1 = serial.Serial('COM3', baudrate=115200) # Initalize the serial port.
+uart1.write(bytes([0xA5, 0x25])) # Send command to stop scanning.
+sleep(0.1) # Wait for the scan to stop.
+uart1.flushInput() # Clears the input buffer on the serial port.
+uart1.write(bytes([0xA5, 0x82, 0x05, 0x01, 0x00, 0x00, 0x00, 0x00, 0x23])) # Send command to start scanning in boost mode.
