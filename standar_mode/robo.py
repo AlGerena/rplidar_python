29b043d0c350e50efeef6d7ad4365ff93e74ef4a
AlGerena <alexisgerena@outlook.es>
2023-12-12 13:07:18 -0500

    Blame

--- standar_mode/robo.py
+++ standar_mode/robo.py
@@ -7,25 +7,24 @@ sleep(0.002) # Waits for the Response Descriptor (must be > 0.002).
 print(uart1.read(7)) # Read the Response Descriptot.
 sleep(0.8)# Waits for the Response data (must be > 0.8 to 0x20).
 
-a = 1
-while a<=1000:
-    #a += 1
+while True:
     message = uart1.read(5)
     if len(message) == 5:
         if (message[0] & 0b11) == 1:
-            print('new scan')
+            print('New scan.')
         elif (message[0] & 0b11) == 2:
-            print('current scan')
+            print('Current scan')
         else :
-            print('error in samplimg')
-            a = 200
+            print('Error in samplimg.')
+            break
         print(bin(message[2]), bin(message[1]), bin(message[0]), bin(message[0] & 0b11))
         angl = ( (message[2]<< 7)) | (message[1] >> 1)
         dist = (message[4] << 8) | (message[3])
         print(bin(angl), angl, len(bin(angl)))
         print('Angulo:', angl/64, 'Distancia:', dist/40)
     else:
-        a = 20000
+        print('Message size is invalid.')
+        break
 uart1.write(bytes([0xA5, 0x25]))
 sleep(0.5)
 uart1.read()
