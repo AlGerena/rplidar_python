71f4af46dd69add6eeafd2118eaabfcdc1e5d50a
AlGerena <alexisgerena@outlook.es>
2024-03-07 22:54:18 -0500

    Blame

--- standar_mode/standard.py
+++ standar_mode/standard.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
 
-uart1 = serial.Serial('/dev/ttyUSB0', baudrate=115200) # Initalize the serial port.
+uart1 = serial.Serial('COM5', baudrate=115200) # Initalize the serial port.
 uart1.write(bytes([0xA5, 0x25])) # Send command to stop scanning.
 sleep(0.1) # Wait for the scan to stop.
 uart1.flushInput() # Clears the input buffer on the serial port.
